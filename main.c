9a94351db53adaf6ec02ad93d89a8f69ce4a84ba
euun1 <daeuun1@gmail.com>
2023-10-15 23:56:16 +0900

    4-1

--- main.c
+++ main.c
@@ -3,39 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b)
+void square(int a)
 {
-	return a+b;
-}
-
-int square(int n) 
-{
-    int r = n * n;
-    return r;
-}
-
-int get_max(int x, int y) 
-{
-    if (x > y) {
-        return x;
-    } else {
-        return y;
-    }
+	a=a*a;
 }
 
 int main(void) {
-	int num1=2;
-	int num2=3;
-	
-	int result = sumTwo(num1, num2);
-	printf("%d\n hello world!\n", result);
-	
-	int s1 = square(num1);
-    int s2 = square(num2);
-    printf("%d\n%d\n hello world!\n", s1, s2);
-    
-    int max = get_max(num1, num2);
-    printf("%d\n hello world!\n", max);
+	int a=2;
+	square(a);
+	printf("a=%i\n", a);
 	
 	system("PAUSE");
 	return 0;
